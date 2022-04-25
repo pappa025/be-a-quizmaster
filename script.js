49ee2d2283f5cb162caf9635a841ee0a329a46c4
Adam Papp <pappadam025@gmail.com>
2022-04-25 22:40:53 +0000

    the script is done for the first question

---
+++ script.js
@@ -0,0 +1,56 @@
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById ('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+console.log('Started')
+startButton.classList.add('hide')
+shuffledQuestions = questions.sort(() => Math.random() - .5)
+currentQuestionIndex = 0
+questionContainerElement.classList.remove('hide')
+setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+        button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsEleement.removeChild(answerButtonElement.firstChild)
+    }
+}
+function selectAnswer(e) {
+
+}
+
+const questions = [
+    {
+        question: 'What is 2+2',
+        ansers: [
+            { text: '4', correct: true},
+            { text: '234', correct: false}
+        ]
+    }
+]
