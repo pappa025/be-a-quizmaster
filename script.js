c99cef414e28b6ef9bfc84ce99d4efc7cf79f725
Adam Papp <pappadam025@gmail.com>
2022-04-27 19:31:01 +0000

    restructure js to be simplier

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById ('question-container')
@@ -5,7 +6,56 @@ const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
+const questions = [
+    {
+        question: 'What is 1+2',
+        answers: [
+            { text: '4', correct: true},
+            { text: '234', correct: false}
+            { text: '234', correct: false}
+            { text: '234', correct: false}
+        ]
+    },
+    {
+        question: 'What is 2+2',
+        answers: [
+            { text: '4', correct: true},
+            { text: '234', correct: false}
+            { text: '234', correct: false}
+            { text: '234', correct: false}
+        ]
+    },
+    {
+        question: 'What is 3+2',
+        answers: [
+            { text: '4', correct: true},
+            { text: '234', correct: false}
+            { text: '234', correct: false}
+            { text: '234', correct: false}
+        ]
+    },
+    {
+        question: 'What is 4+2',
+        answers: [
+            { text: '4', correct: true},
+            { text: '234', correct: false}
+            { text: '234', correct: false}
+            { text: '234', correct: false}
+        ]
+    },
+    {
+        question: 'What is 5+2',
+        answers: [
+            { text: '4', correct: true},
+            { text: '234', correct: false}
+            { text: '234', correct: false}
+            { text: '234', correct: false}
+        ]
+    },
+]
+
 startButton.addEventListener('click', startGame)
+
 
 function startGame() {
 console.log('Started')
@@ -15,6 +65,7 @@ currentQuestionIndex = 0
 questionContainerElement.classList.remove('hide')
 setNextQuestion()
 }
+
 
 function setNextQuestion() {
     resetState()
@@ -45,13 +96,55 @@ function resetState() {
 function selectAnswer(e) {
 
 }
-
+*/
 const questions = [
     {
-        question: 'What is 2+2',
-        ansers: [
-            { text: '4', correct: true},
-            { text: '234', correct: false}
-        ]
-    }
+            question: "How many players are allowed on a soccer pitch ?",
+            option1: "10 players",
+            option2: "11 players",
+            option3: "9 players",
+            option4: "12 players",
+            correctOption: "option3"
+    },
+    {
+        question: "How many players are allowed on a soccer pitch ?",
+        option1: "10 players",
+        option2: "11 players",
+        option3: "9 players",
+        option4: "12 players",
+        correctOption: "option3"
+},
+{
+    question: "How many players are allowed on a soccer pitch ?",
+    option1: "10 players",
+    option2: "11 players",
+    option3: "9 players",
+    option4: "12 players",
+    correctOption: "option3"
+},
+{
+    question: "How many players are allowed on a soccer pitch ?",
+    option1: "10 players",
+    option2: "11 players",
+    option3: "9 players",
+    option4: "12 players",
+    correctOption: "option3"
+},
 ]
+
+let questionNumber = 1
+let playerScore = 0
+let wrongAttempt = 0
+let indexNumber = 0
+
+let shuffledQuestion = [] 
+function handleQuestion() { 
+    while (shuffledQuestions.length <= 5) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
